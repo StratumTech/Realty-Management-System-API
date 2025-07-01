3d1f631036a7551f4f72c5e1535ed7d54f0eaee6
Pukhov Kirill <kirillpuhov526@gmail.com>
2025-07-01 12:23:31 +0300

    feat: create a project structure

---
+++ src/main/java/com/stratumtech/realtyapi/RealtyManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.stratumtech.realtyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyManagementSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtyManagementSystemApiApplication.class, args);
+    }
+
+}
